d93a2920f40f4ab4008ca5d7304f996cce9ac282
m1 <uzbzarif@gmail.com>
2024-07-13 10:39:20 +0500

    get json

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
 
+
 url = 'https://randomuser.me/api/'
 
 response = requests.get(url)
-# print status code
-print(response.status_code)
-# print type of response
-print(type(response))
+
+# Get full name 
+# Get email
+# Get phone number
+# Get address
+data = response.json()
+print(data['results'][0]['name']['first'])
+
+    
+
+
